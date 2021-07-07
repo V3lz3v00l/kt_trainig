1774d687e7be9bd9560ab100023071def1213bd8
Mish <crowdford@gmail.com>
2021-07-07 05:16:04 +0300

    added quick sort in Sort class

--- src/main/kotlin/Sort.kt
+++ src/main/kotlin/Sort.kt
@@ -1,6 +1,33 @@
+import kotlin.random.Random as Random1
+
 /**
  * Sorting elements of array with several algorithms
  */
 class Sort {
-    
+    fun generateUnsortedInts(): MutableList<Int> {
+        return MutableList(30) { kotlin.random.Random.nextInt(0, 100) }
+    }
+
+    fun qSort(array: MutableList<Int>, low: Int, high: Int) {
+        var i = low
+        var j = high
+
+        val tmpArrayEl = array[low + (high - low) / 2]
+
+        while (i <= j) {
+            while (array[i] < tmpArrayEl) i++
+            while (array[j] > tmpArrayEl) j--
+            if (i <= j) {
+                val temp: Int = array[i]
+                array[i] = array[j]
+                array[j] = temp
+                i++
+                j--
+            }
+        }
+
+        if (low < j) qSort(array, low, j)
+        if (i < high) qSort(array, i, high)
+    }
+
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,16 @@
 import kotlinx.coroutines.*
+import java.util.*
 
 fun main(args: Array<String>) {
-    val bin = BinarySearch()
-    println(bin.findVertexIndexIterate(bin.array))
-    println(bin.findVertexIndex(bin.array, bin.array))
+//    val bin = BinarySearch()
+//    println(bin.findVertexIndexIterate(bin.array))
+//    println(bin.findVertexIndex(bin.array, bin.array))
 
+    val sort = Sort()
+    var array = sort.generateUnsortedInts()
+    print("before sort")
+    println(array)
+    sort.qSort(array, 0, array.size - 1)
+    print("after sort")
+    println(array)
 }
